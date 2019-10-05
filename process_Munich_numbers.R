700187e3735da6b7ac8be36d4c37c5a0f8ef1205
Calin Uioreanu <prophp@gmail.com>
2019-10-05 19:28:14 +0200

    create large data matrix, exploring correlated monthly values

--- process_Munich_numbers.R
+++ process_Munich_numbers.R
@@ -7,26 +7,21 @@ url = 'http://www.mstatistik-muenchen.de/monatszahlenmonitoring/export/xlsx/mzm_
 destExcelFile = "file.xlsx"
 
 library(RCurl)
-#x <- getURL(url)
 
 download.file(url = url,
               destfile=destExcelFile,
               method="libcurl",
               mode = "wb")
 
-# library(xlsx)
-# dat <- read.xlsx(destExcelFile, 1, header=TRUE)
-# 
-# sheetName = dat$THEMA[1]
-# dat1 <- read.xlsx(destExcelFile, sheetName = sheetName, header=TRUE)
-
-
+library(readxl)
 library(tidyverse)
 path = destExcelFile
 dat <- path %>% 
   excel_sheets() %>% 
   set_names() %>% 
   map(read_excel, path = path)
+
+rm(path, destExcelFile, url)
 
 dat$KINOS %>%
   select(MONATSZAHL) %>%
@@ -38,7 +33,9 @@ dat$KINOS %>%
     geom_line() + 
     facet_wrap(~AUSPRAEGUNG) +
     theme_classic()+ 
-    geom_smooth() + scale_y_continuous(labels = scales::comma) + ggtitle("monthly trend")
+    geom_smooth() + scale_y_continuous(labels = scales::comma) + ggtitle("monthly trend - Cinema Visitors")
+ggsave("monthly trend - Cinema Visitors.png", dpi=400, dev='png', height=4, width=5, units="in", scale = 2)
+
 
 dat$WITTERUNG %>%
   filter(MONATSZAHL %in% c("Sonnenschein", "Lufttemperatur")) %>%
@@ -47,7 +44,8 @@ dat$WITTERUNG %>%
   geom_line() + 
   facet_wrap(~AUSPRAEGUNG, scales = "free") +
   theme_classic()+ 
-  geom_smooth() + scale_y_continuous(labels = scales::comma) + ggtitle("monthly trend")
+  geom_smooth() + scale_y_continuous(labels = scales::comma) + ggtitle("monthly weather trends")
+ggsave("monthly weather trends.png", dpi=400, dev='png', height=4, width=5, units="in", scale = 2)
 
 
 dat$TOURISMUS %>%
@@ -57,16 +55,9 @@ dat$TOURISMUS %>%
   geom_line() + 
   facet_wrap(~AUSPRAEGUNG, scales = "free") +
   theme_classic()+ 
-  geom_smooth() + scale_y_continuous(labels = scales::comma) + ggtitle("monthly trend")
+  geom_smooth() + scale_y_continuous(labels = scales::comma) + ggtitle("monthly trend - Turist guests")
+ggsave("monthly trend - Turist guests.png", dpi=400, dev='png', height=4, width=5, units="in", scale = 2)
 
-dat$TOURISMUS %>%
-  filter(MONATSZAHL %in% c("Übernachtungen")) %>%
-  mutate(Month = as.Date(paste0(MONAT,'01'), format = '%Y%m%d')) %>%
-  ggplot(., aes(x=Month, y = WERT, group=AUSPRAEGUNG)) + 
-  geom_line() + 
-  facet_wrap(~AUSPRAEGUNG, scales = "free") +
-  theme_classic()+ 
-  geom_smooth() + scale_y_continuous(labels = scales::comma) + ggtitle("monthly trend")
 
 dat$BEVÖLKERUNG %>%
   filter(MONATSZAHL %in% c("Familienstand")) %>%
@@ -75,5 +66,57 @@ dat$BEVÖLKERUNG %>%
   geom_line() + 
   facet_wrap(~AUSPRAEGUNG, scales = "free") +
   theme_classic()+ 
-  scale_y_continuous(labels = scales::comma) + ggtitle("monthly trend")
+  scale_y_continuous(labels = scales::comma) + ggtitle("monthly trend - population by family status")
+ggsave("monthly trend - population by family status.png", dpi=400, dev='png', height=4, width=5, units="in", scale = 2)
 
+
+# looping through all the Excel sheets
+dsFinal <- data.frame()
+for (i in 1:length(names(dat))) {
+  SECTIONNAME <- names(dat)[i]
+  dtmp <- (dat[SECTIONNAME])[[1]]
+  # if the current section has values in it
+  if ('WERT' %in% names(dtmp)) {
+    # creating dynamic column names and assign monthly values as cols
+    dtmp_mo <- dtmp %>%
+      select(MONATSZAHL, AUSPRAEGUNG, MONAT, WERT) %>%
+      mutate(colname=paste(SECTIONNAME, MONATSZAHL, AUSPRAEGUNG, sep = " - ")) %>%
+      select(MONAT, colname, WERT) %>%
+      spread(MONAT, WERT) 
+    rownames(dtmp_mo) <- dtmp_mo$colname
+    dtmp_mo$colname <- NULL
+    # transpose monthly values to rows
+    dtmp_mo <- t(dtmp_mo)
+    # back from tibble to dataframe
+    dtmp_mo <-as.data.frame(dtmp_mo)
+    dtmp_mo$month <- rownames(dtmp_mo)
+    
+    if (nrow(dsFinal)==0){
+      # if the final construct doesn't exist, simply create it
+      dsFinal <- dtmp_mo
+    } else {
+      # if we have values in it already, merge them (full join)  using the month column
+      dsFinal <- dtmp_mo %>%
+        full_join(dsFinal, by = "month")
+    }
+  }
+}
+# cleanup
+rm(dtmp, dtmp_mo, i, SECTIONNAME)
+
+rownames(dsFinal) <- dsFinal$month
+dsFinal$month <- NULL
+str(dsFinal)
+
+dsCor <- cor(dsFinal, use = "pairwise.complete.obs")
+
+dsCor[1:5, 1:5]
+
+# explore correlations with R https://drsimonj.svbtle.com/exploring-correlations-in-r-with-corrr
+diag(dsCor) <- NA
+col_has_over_90 <- apply(dsCor, 2, function(x) any(x > .9))
+ 
+View( dsCor[,col_has_over_90] )
+
+library(corrr)
+correlate(dsFinal, use = "pairwise.complete.obs")
